ca97852e49dca8f3b75da6f3490bd72fb5e69697
JoeSenna <joe.senna@live.com>
2022-01-12 19:27:22 -0300

    Criação das páginas funcionais

---
+++ conversor.php
@@ -0,0 +1,19 @@
+<?php
+
+    $fah = $_POST['fah']; 
+    $cel = (($fah-32)*5)/9;
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor</title>
+</head>
+<body>
+    <h1><?php echo $cel ?></h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor</title>
+</head>
+<body>
+    <form action="conversor.php" method="POST">
+        <h2>Conversor</h2>
+        <input type="number" name="fah" placeholder="Temperatura em Fahrenheirt">
+        <input type="submit" value="Converter">
+    </form>
+</body>
+</html>
